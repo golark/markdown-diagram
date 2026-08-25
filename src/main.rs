83912e5d6d49d1433387ca4846dc07998af0d274
can <firatcnur@gmail.com>
2026-08-25 15:39:29 +0100

    node and arrows

--- src/main.rs
+++ src/main.rs
@@ -38,9 +38,27 @@ fn main() {
     };
     eprintln!("file read, {} bytes", content.len());
 
-    let mut nodes = Node::new("x".to_string());
+    let mut nodes = Vec::new();
     println!("{:?}", nodes);
 
-    //let re = Regex::new(r"(?P<prefix>[│├└─\s]*)(?P<arrow>[>v^])?\s*(?P<text>\[.*?\])").unwrap();
-    //eprintln!("regex compiled");
+    let re = Regex::new(r"(?P<prefix>[│├└─\s]*)(?P<arrow>[>v^])?\s*(?P<text>\[.*?\])").unwrap();
+
+    for captures in re.captures_iter(&content) {
+        let prefix = captures.name("prefix").map_or("", |m| m.as_str());
+        let arrow = captures.name("arrow").map_or("", |m| m.as_str());
+        let text = captures.name("text").map_or("", |m| m.as_str());
+
+        // if node is empty, create a new node and add it to the nodes vector
+        if nodes.is_empty() {
+            let new_node = Node::new(text.to_string());
+            nodes.push(Box::new(new_node));
+        } else {
+            // if node is not empty, add the new node as a child of the last node
+            let last_node = nodes.last_mut().unwrap();
+            let new_node = Node::new(text.to_string());
+            last_node.arrows.push(Box::new(new_node));
+        }
+
+        println!("Prefix: '{}', Arrow: '{}', Text: '{}'", prefix, arrow, text);
+    }
 }
